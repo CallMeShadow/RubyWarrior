0643e764d8425e8f4f66c843fce187b0b441e713
CallMeShadow <AlekseyVicLarichkin@gmail.com>
2013-11-26 22:31:36 +0200

    Create Level7.rb

---
+++ Level7.rb
@@ -0,0 +1,44 @@
+class Player
+  def play_turn(warrior)
+  if warrior.feel.wall? == true
+    warrior.pivot!
+  else  
+    if warrior.health > 13
+     if warrior.feel.empty? == true
+      if warrior.health < 20
+        if @health <= warrior.health
+          warrior.rest!
+        else
+          warrior.walk!
+        end
+      else
+        warrior.walk!
+      end
+    else
+      warrior.attack!
+      end
+       
+    else
+    
+      if warrior.feel(:backward).empty? == true
+      
+          warrior.walk!(:backward)
+          
+      else 
+      
+          if warrior.feel(:backward).captive? == true
+          
+              warrior.rescue!(:backward)
+              
+          else
+          
+             warrior.rest!
+      
+          end
+      end
+      
+    end  
+    @health=warrior.health
+    end
+  end
+end
